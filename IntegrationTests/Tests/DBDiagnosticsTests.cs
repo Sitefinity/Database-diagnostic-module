ef1730e227783e3c86e14285640dad0cf0a19cb8
Orlin Draganov <draganov@progress.com>
2019-08-15 11:38:49 +0300

    Renamed readme and integration tests in csproj, removed unneeded tests

--- IntegrationTests/Tests/DBDiagnosticsTests.cs
+++ IntegrationTests/Tests/DBDiagnosticsTests.cs
@@ -34,44 +34,5 @@ namespace IntegrationTests.Tests
             Assert.IsNotNull(dbSize.Name);
             Assert.IsNotNull(dbSize.Size);
         }
-
-        [Test]
-        [Category(TestCategories.DBDiagnostics)]
-        [Author("Progress Sitefinity")]
-        [Description("A simple test that will pass.")]
-        public void PassingTest()
-        {
-            int expectedValue = 2;
-
-            int actualValue = 1 + 1;
-
-            Assert.AreEqual<int>(expectedValue, actualValue);
-        }
-
-        [Test]
-        [Category(TestCategories.DBDiagnostics)]
-        [Author("Progress Sitefinity")]
-        [Description("A simple test that will fail.")]
-        public void FailingTest()
-        {
-            int expectedValue = 2;
-
-            int actualValue = 1 + 1 + 1;
-
-            Assert.AreEqual<int>(expectedValue, actualValue);
-        }
-
-        [Test]
-        [Category(TestCategories.DBDiagnostics)]
-        [Author("Progress Sitefinity")]
-        [Description("A simple test that will be ignored. You can run it manually in the Test runner.")]
-        public void IgnoredTest()
-        {
-            int expectedValue = 2;
-
-            int actualValue = 1 + 1;
-
-            Assert.AreEqual<int>(expectedValue, actualValue);
-        }
     }
 }
